31395e38bc0598006d87e75c4e16faf2b272b7e0
moscmaniaco <34272971+moscmaniaco@users.noreply.github.com>
2019-05-27 19:12:08 +0200

    Carrera en marcha

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 class Circuito():
     corredores=[]
@@ -12,8 +13,8 @@ class Circuito():
         self.__screen = turtle.Screen()
         self.__screen.setup(width,height)
         self.__screen.bgcolor('lightgray')
-        self.__starline = -width/2 +20
-        self.__finisline = width/2 -20
+        self.__starline = -width/2 +60
+        self.__finisline = width/2 -60
         
         self.__createRunners()
       
@@ -28,6 +29,19 @@ class Circuito():
             
             self.corredores.append(new_turtle)
             
+    def Competir(self):
+        
+        hayGanador = False
+        
+        while not hayGanador:
+            for tortuga in self.corredores:
+                avance =random.randint(1,6)
+                tortuga.forward(avance)
+                
+                if tortuga.position()[0] >= self.__finisline:
+                    hayGanador = True
+                    print("La tortuga de color {} ha ganado".format(tortuga.color()[0]))
         
 if __name__ == '__main__':
     circuito=Circuito(640,480)
+    circuito.Competir()
